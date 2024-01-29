e0915aef5d278595234934583c0967136dd0d96a
Michael Michel <michael.spiegelhalter@gmx.de>
2024-01-29 22:51:51 +0100

    removed tilemap added rapier2d

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
 use bevy::prelude::*;
 use bevy::window::*;
-use bevy_ecs_tilemap::prelude::*;
 
 
 mod entities;
 mod animation;
-mod tilemap;
 
 fn main() {
     App::new()
@@ -17,13 +15,9 @@ fn main() {
             }),
             ..default()
         }))
-        .add_plugins(TilemapPlugin)
         .add_systems(Startup, spawn_camera)
-        //.add_systems(Startup, spawn_tilemap)
         .add_systems(Startup, entities::player::spawn_player)
         .add_systems(Update, animation::animation::animate_entity)
-        //.add_systems(Update, swap_texture_or_hide)
-        //.add_systems(Update, get_tilemap)
         .run();
 }
 
@@ -36,30 +30,4 @@ pub fn spawn_camera(mut commands: Commands, window_query: Query<&Window, With<Pr
     });*/
 
     commands.spawn(Camera2dBundle::default());
-}
-
-fn swap_texture_or_hide(
-    asset_server: Res<AssetServer>,
-    keyboard_input: Res<Input<KeyCode>>,
-    mut query: Query<(&mut TilemapTexture, &mut Visibility)>,
-) {
-    if keyboard_input.just_pressed(KeyCode::Space) {
-        let texture_a = TilemapTexture::Single(asset_server.load("tiles.png"));
-        let texture_b = TilemapTexture::Single(asset_server.load("tiles2.png"));
-        for (mut tilemap_tex, _) in &mut query {
-            if *tilemap_tex == texture_a {
-                *tilemap_tex = texture_b.clone();
-            } else {
-                *tilemap_tex = texture_a.clone();
-            }
-        }
-    }
-    if keyboard_input.just_pressed(KeyCode::H) {
-        for (_, mut visibility) in &mut query {
-            *visibility = match *visibility {
-                Visibility::Inherited | Visibility::Visible => Visibility::Hidden,
-                Visibility::Hidden => Visibility::Visible,
-            };
-        }
-    }
 }
